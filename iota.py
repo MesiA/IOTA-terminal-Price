2f60b33a76e6449bbead4bb3d6e943f81dfbcbcc
Ali Mohammad Pur <Ali.mpfard@gmail.com>
2017-12-29 14:24:16 +0330

    made formats not terrible

--- iota.py
+++ iota.py
@@ -12,29 +12,30 @@ clear = lambda: os.system(c)
 d = float(input('Enter Delay time in seconds:'))
 d = 1 if d<1 else d
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+bcolors = {
+    header_c : '\033[95m'
+    okblue_c : '\033[94m'
+    okgreen_c : '\033[92m'
+    warning_c : '\033[93m'
+    fail_c : '\033[91m'
+    end_c : '\033[0m'
+    bold_c : '\033[1m'
+    underline_c : '\033[4m'
+}
 temp = 0
 while 1:
     url = urllib.request.urlopen("https://api.coinmarketcap.com/v1/ticker/iota/")
     data = json.loads(url.read().decode())
     clear()
-    print(bcolors.BOLD+bcolors.UNDERLINE+bcolors.OKBLUE+"\t\tIOTA PRICE Monitor"+bcolors.ENDC)
-    color = bcolors.WARNING if temp > float(data[0]['price_usd']) else bcolors.OKGREEN
+    print("{bold_c}{underline_c}{okblue_c}\t\tIOTA PRICE Monitor{end_c}".format(**bcolors))
+    color = bcolors['warning+c'] if temp > float(data[0]['price_usd']) else bcolors['okgreen_c']
     temp = float(data[0]['price_usd'])
     print(
-        '''  {4}Price (USD):{5}{7}{0}{5}
-  {4}Change (1h):{5}{6}{1}%{5}
-  {4}Change (1d):{5}{6}{2}%{5}
-  {4}Change (1w):{5}{6}{3}%{5}
-    '''.format(data[0]['price_usd'],data[0]['percent_change_1h'],data[0]['percent_change_24h'],data[0]['percent_change_7d'],bcolors.HEADER,bcolors.ENDC,bcolors.BOLD,color))
-    print('Please join us at:{0} @ai_python{1}'.format(bcolors.OKGREEN,bcolors.ENDC))
-    print('\tupdated at:',datetime.datetime.now())
+        '''  {header_c}Price (USD):{end_c}{color}{price_usd}{end_c}
+  {bold_c}Change (1h):{end_c}{bold_c}{price_change_1h}%{end_c}
+  {bold_c}Change (1d):{end_c}{bold_c}{percent_change_24h}%{end_c}
+  {bold_c}Change (1w):{end_c}{bold_c}{percent_change_7d}%{end_c}
+    '''.format(**data[0], **bcolors))
+    print('Please join us at:{okgreen_c} @ai_python{end_c}'.format(**bcolors))
+    print('\tlast updated at:',datetime.datetime.now())
     time.sleep(int(d))
